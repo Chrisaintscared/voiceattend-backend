d39320108596aa61ecb6891be172fc0bc0622eec
Chrisaintscared <jhoncatubig2@gmail.com>
2026-04-24 14:39:49 +0800

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -232,3 +232,20 @@ def list_all_users():
 
     finally:
         conn.close()
+
+def delete_user(user_id: int):
+    conn = get_connection()
+    try:
+        cur = conn.cursor(cursor_factory=RealDictCursor)
+
+        cur.execute("""
+            DELETE FROM users
+            WHERE id = %s
+            RETURNING id, name, email, role;
+        """, (user_id,))
+
+        conn.commit()
+        return cur.fetchone()
+
+    finally:
+        conn.close()
