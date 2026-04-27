3b01f47e5332630a22e96f6c542ab41c063487f2
Chrisaintscared <jhoncatubig2@gmail.com>
2026-04-27 10:25:16 +0800

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,7 +3,6 @@ from fastapi.middleware.cors import CORSMiddleware
 import traceback
 
 app = FastAPI(title="VoiceAttend AI", version="1.0.0")
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -24,12 +23,11 @@ def startup():
 
 # import routes safely
 try:
-    from app.routes import auth, admin, attendance
-
+    from app.routes import auth, admin, attendance, classes
     app.include_router(auth.router, prefix="/auth", tags=["auth"])
     app.include_router(admin.router, prefix="/admin", tags=["admin"])
     app.include_router(attendance.router, prefix="/attendance", tags=["attendance"])
-
+    app.include_router(classes.router, prefix="/classes", tags=["classes"])
     print("✅ Routes loaded")
 except Exception as e:
     print("❌ Route import failed:", e)
