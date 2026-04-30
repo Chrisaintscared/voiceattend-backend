c4083ccfc78b1fdf3b2b84cfab1dcb633e8d1b99
Chrisaintscared <jhoncatubig2@gmail.com>
2026-04-30 10:30:33 +0800

    Update __init__.py

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -1,2 +1,9 @@
-# app/routes/__init__.py
+from fastapi import APIRouter
 from . import auth, admin, attendance, classes, enroll
+
+router = APIRouter()
+router.include_router(auth.router, prefix="/auth")
+router.include_router(admin.router, prefix="/admin")
+router.include_router(attendance.router, prefix="/attendance")
+router.include_router(classes.router, prefix="/classes")
+router.include_router(enroll.router, prefix="/enroll")
