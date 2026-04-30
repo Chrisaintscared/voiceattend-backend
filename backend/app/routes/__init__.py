fb4032168240f74c299022a691c6c6ca154b3ee8
Chrisaintscared <jhoncatubig2@gmail.com>
2026-04-30 10:20:09 +0800

    Update __init__.py

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -1,5 +1,2 @@
-from .auth import router as auth
-from .admin import router as admin
-from .attendance import router as attendance
-from .classes import router as classes
-from .enroll import router as enroll
+# app/routes/__init__.py
+from . import auth, admin, attendance, classes, enroll
